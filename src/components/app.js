7201b464a2014d041b27646226cf833e7f3bbe45
Autumn <autumncbullard@gmail.com>
2018-07-26 19:50:22 -0400

    Declare variables to assign links to audio files

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 
+const clap = require('.././audio/clap.wav');
+const hihat = require('.././audio/hihat.wav');
+const kick = require('.././audio/kick.wav');
+const openhat = require('.././audio/openhat.wav');
+const boom = require('.././audio/boom.wav');
+const ride = require('.././audio/ride.wav');
+const snare = require('.././audio/snare.wav');
+const tom = require('.././audio/tom.wav');
+const tink = require('.././audio/tink.wav');
+
 export default class App extends Component {
 
   constructor(props) {
