2b1b308d29f27af0c953506d4bc36e2e34f9c9cb
Autumn <autumncbullard@gmail.com>
2018-10-05 15:39:58 -0400

    Add event to the button onClick

--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ button, playSound }) => {
 
   return (
-    <button type="button" className="drum-pad" onClick={() => playSound(event, button.key)} id={button.type}>{button.key}<audio src={button.sound} className="clip" id={button.key} /></button>
+    <button type="button" className="drum-pad" onClick={(event) => playSound(event, button.key)} id={button.type}>{button.key}<audio src={button.sound} className="clip" id={button.key} /></button>
   );
 }
 
